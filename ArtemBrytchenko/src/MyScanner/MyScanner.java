bf23e6c735b330f86ee8066e0e435f11c8224055
BrArty <brtartemiy@ukr.net>
2016-02-26 20:54:47 +0200

    change next() method

--- ArtemBrytchenko/src/MyScanner/MyScanner.java
+++ ArtemBrytchenko/src/MyScanner/MyScanner.java
@@ -1,35 +1,87 @@
 package MyScanner;
 
 import java.io.*;
+import java.util.InputMismatchException;
 
 public class MyScanner implements Closeable{
 
     private Reader reader;
-    private StringBuilder dataLine = new StringBuilder();
+    private StringBuilder dataLine;
     private char[] buffer = new char[1024];
+    private char delimiter = ' ';
+    private Integer intData;
+    private Integer[] intBuffer;
+    private int endIndex = 0;
 
     public MyScanner(InputStream is) {
         reader = new InputStreamReader(is);
     }
 
-    public MyScanner(File file) throws FileNotFoundException {
-        reader = new InputStreamReader(new FileInputStream(file));
+    public MyScanner(String filePath) throws FileNotFoundException {
+        reader = new InputStreamReader(new FileInputStream(new File(filePath)));
+    }
+
+    public void read(){
+        dataLine = new StringBuilder();
+        try {
+            do {
+                reader.read(buffer, 0, buffer.length);
+                dataLine.append(buffer);
+                endIndex++;
+            } while (buffer.length != 1024);
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public StringBuilder getDataLine() {
+        return dataLine;
     }
 
     public String next() throws IOException {
-        int index = 0;
-        while (isEndOfWord(index)) {
-             index++;
+        read();
+        for (int i = 0; i < dataLine.length();) {
+            if (dataLine.substring(i, i+1).equals(" ")) {
+                return dataLine.substring(0, i);
+            } else i++;
         }
-        return String.valueOf(buffer).trim();
+        return String.valueOf(dataLine);
     }
 
-    private boolean isEndOfWord(int index) throws IOException {
-        return (buffer[index] = (char) reader.read()) != ' ';
+    /*public int nextInt() throws IOException {
+        if (hasNextInt()){
+            for (int i = 0; i < buffer[endIndex]; i++){
+                if (buffer[i] >= '\u0030' && buffer[i] <= '\u0039'){
+                    intBuffer[i] = (int) buffer[i];
+                }
+            }
+
+        }else throw new InputMismatchException();
+    }*/
+
+    public boolean hasNext(){
+        read();
+        return dataLine.length() != 0;
     }
 
-    public int nextInt(){
-        return 0;
+    public boolean hasNextInt() throws IOException {
+        try {
+            intData = new Integer(next());
+            resetBuffer();
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private void resetBuffer(){
+        dataLine = new StringBuilder();
     }
 
     @Override
--- ArtemBrytchenko/src/MyScanner/TestScanner.java
+++ ArtemBrytchenko/src/MyScanner/TestScanner.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 
 public class TestScanner {
     public static void main(String[] args) throws IOException {
-        MyScanner myScanner = new MyScanner(System.in);
+        MyScanner myScanner = new MyScanner("C:/Users/ARTEM/IdeaProjects/ACP12/ArtemBrytchenko/src/MyScanner/text_file");
+//        MyScanner myScanner = new MyScanner(System.in);
         System.out.println(myScanner.next());
     }
 }
