ece9f60f63c9d43235eeb3662b69f4a1b429bfe2
RomaBlaSo <voloshyn.roman@gmail.com>
2016-02-27 09:10:48 +0200

    2016_02_26 20:40 - MyScanner

--- RomanVoloshyn/src/myScanner/MyScanner.java
+++ RomanVoloshyn/src/myScanner/MyScanner.java
@@ -7,22 +7,25 @@ import java.util.regex.Pattern;
 
 public class MyScanner {
 
+    private int DEFAULT_SIZE_IN_BYTES = 1024;
+
     //собственно объект чтения
     private boolean inputReaderIsEmpty = false;
     private Reader inputReader;
 
     //подкачка символов из объекта
-    private char[] bufChar = new char[1024];
-    //private int countReading = 0;
+    private char[] bufChar = new char[DEFAULT_SIZE_IN_BYTES];
+    private int globalByteCounter = 0;
 
     //буфер строки
     private int indexBuf = 0;
-    private StringBuilder bufStrBuilder = new StringBuilder();
+    private StringBuilder bufStrBuilder = new StringBuilder(DEFAULT_SIZE_IN_BYTES);
 
     //паттерны
     private Pattern PATTERN_DELIMITER = Pattern.compile("\\s+");
     private Pattern PATTERN_INT = Pattern.compile("\\d+");
     private Pattern PATTERN_DELIMITER_ROWS = Pattern.compile("\\n");
+
 
 //    public MyScanner(InputStream inputStream){
 //        this.inputStream = inputStream;
@@ -30,10 +33,15 @@ public class MyScanner {
 
     public MyScanner(Reader inputReader) throws IOException {
         this.inputReader = inputReader;
-        pumpNextPart();
+        pumpNextPart(false);
     }
 
-    private boolean pumpNextPart() throws IOException {
+    public MyScanner(String inputString) throws IOException {
+        inputReaderIsEmpty = true;
+        bufStrBuilder.append(inputString);
+    }
+
+    private boolean pumpNextPart(boolean trimAllow) throws IOException {
         if (inputReaderIsEmpty) {
             return false;
         }
@@ -44,8 +52,16 @@ public class MyScanner {
             return false;
         }
 
+        if (trimAllow && indexBuf >= DEFAULT_SIZE_IN_BYTES) {
+            String currentString = bufStrBuilder.substring(indexBuf);
+            bufStrBuilder = new StringBuilder(DEFAULT_SIZE_IN_BYTES + currentString.length());
+            bufStrBuilder.append(currentString);
+            indexBuf = 0;
+        }
+
+        globalByteCounter += counter;
         bufStrBuilder.append(bufChar);
-        if (counter < 1024) {
+        if (counter < DEFAULT_SIZE_IN_BYTES) {
             int index = bufStrBuilder.indexOf("\u0000");
             if (index != -1) {
                 bufStrBuilder.delete(bufStrBuilder.indexOf("\u0000"), bufStrBuilder.length());
@@ -56,36 +72,94 @@ public class MyScanner {
     }
 
     //чтение вхождения по PATTERN_DELIMITER
-    public String next() throws IOException, InterruptedException {
+    public String nextByPattern(Pattern pattern) throws IOException, InterruptedException {
+
+        Matcher matcher = findMatcher(pattern);
+        if (matcher == null) {
+            return null;
+        }
+
+        int indexStartString = indexBuf;
+        int indexStartPattern = matcher.start();
+        indexBuf = matcher.end();
+
+        if (indexStartPattern == indexStartString) {
+            return nextByPattern(pattern);
+        }
+
+        return bufStrBuilder.substring(indexStartString, indexStartPattern);
+
+    }
+
+    private Matcher findMatcher(Pattern pattern) throws IOException {
         while (true) {
             String currentString = bufStrBuilder.toString();
-            Matcher matcher = PATTERN_DELIMITER.matcher(currentString);
+            Matcher matcher = pattern.matcher(currentString);
 
             if (!matcher.find(indexBuf)) {
-                if (!pumpNextPart()) {
+                if (!pumpNextPart(true)) {
                     return null;
                 }
-            } else {//что-то нашли
-                currentString = currentString.substring(indexBuf, matcher.start());
-                indexBuf = matcher.end();
-                return currentString;
+            } else {
+                return matcher;
             }
         }
     }
 
-    //определение наличия в потоке данных
-    public boolean hasNext() {
-        return false;
+    public boolean hasNextByPattern(Pattern pattern) throws IOException, InterruptedException {
+        while (true) {
+            String currentString = bufStrBuilder.toString();
+            Matcher matcher = pattern.matcher(currentString);
+
+            if (!matcher.find(indexBuf)) {
+                if (!pumpNextPart(false)) {
+                    return false;
+                }
+            } else {//что-то нашли
+                int index = matcher.start();
+                if (index == indexBuf) {
+                    //в таком варианте проблема будет в таком виде:
+                    //hasNext -> useDelimiter -> hasNext
+                    //часть строки уже может быть откушена
+                    indexBuf = matcher.end();
+                } else {
+                    return !currentString.substring(indexBuf, index).isEmpty();
+                }
+            }
+        }
     }
 
-    // - чтение целого числа из строки или InputMismatchException
-    public int nextInt() {
-        return 0;
+    //чтение по разделителю
+    public String next() throws IOException, InterruptedException {
+        return nextByPattern(PATTERN_DELIMITER);
     }
 
     //чтение всей строки
-    public String nextLine() {
-        return "";
+    public String nextLine() throws IOException, InterruptedException {
+        return nextByPattern(PATTERN_DELIMITER_ROWS);
+    }
+
+    //определение наличия в потоке данных
+    public boolean hasNext() throws IOException, InterruptedException {
+        return hasNextByPattern(PATTERN_DELIMITER);
+    }
+
+    //чтение целого числа из строки или InputMismatchException
+    public int nextInt() throws IOException {
+
+        Matcher matcher = findMatcher(PATTERN_INT);
+        if (matcher == null) {
+            return 0;
+        }
+
+        int indexEnd = matcher.end();
+        if (indexEnd == bufStrBuilder.length() && pumpNextPart(true)) {
+            return nextInt();
+        }
+
+        indexBuf = indexEnd;
+        return Integer.parseInt(bufStrBuilder.substring(matcher.start(), indexEnd));
+
     }
 
     //определение наличия в потоке целого int числа
--- RomanVoloshyn/src/myScanner/TestMyScanner.java
+++ RomanVoloshyn/src/myScanner/TestMyScanner.java
@@ -7,18 +7,37 @@ public class TestMyScanner {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        FileReader fr = new FileReader("C:\\profiles_settings.xml");
+        // http://www.ex.ua/144968917177
+        FileReader fr = new FileReader("C:\\1c_exchange.log");
         MyScanner msc = new MyScanner(fr);
 
+
+        int counter = 0;
+        while (true) {
+            int res = msc.nextInt();
+            if (res == 0) {
+                break;
+            }
+            counter++;
+            System.out.println(res);
+
+        }
+
+        System.out.println(counter);
+
+
+        counter = 0;
         while (true) {
             String res = msc.next();
             if (res == null || res.isEmpty()) {
                 break;
             }
+            counter++;
             System.out.println(res);
 
         }
 
+        System.out.println(counter);
     }
 
 }
