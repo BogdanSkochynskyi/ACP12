6f63a3639ab5125b0dfa4f53e33c98b720ed1ec0
KuzivP <kuziv.petro@ukr.net>
2016-02-28 19:38:17 +0200

    1

---
+++ Kuziv_P/HW_week3_1/src/HTMLParser.java
@@ -0,0 +1,20 @@
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Петро on 28.02.2016.
+ */
+public class HTMLParser {
+    public static void main(String[] args) {
+        List<String>  linksList = new ArrayList<>();
+        Document document =Jsoup.parse("http://www.ex.ua/96058160");
+
+    }
+
+}
