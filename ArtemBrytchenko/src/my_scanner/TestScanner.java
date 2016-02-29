b597e85f4dc42a2c14457d0c056cbff5fc35cc9d
BrArty <brtartemiy@ukr.net>
2016-02-29 17:48:45 +0200

    add working next method

--- ArtemBrytchenko/src/my_scanner/TestScanner.java
+++ ArtemBrytchenko/src/my_scanner/TestScanner.java
@@ -4,34 +4,23 @@ package my_scanner;
 import java.io.IOException;
 
 public class TestScanner {
+
+
     public static void main(String[] args) throws IOException {
         // uncomment to test scanner using file of strings
-/*        MyScanner myScanner = new MyScanner("C:/Users/ARTEM/IdeaProjects/ACP12/ArtemBrytchenko/src/usable_files/string_test");
+        MyScanner myScanner = new MyScanner("C:/Users/ARTEM/IdeaProjects/ACP12/ArtemBrytchenko/src/usable_files/string_test1");
 
-        System.out.println(myScanner.next());
-        System.out.println(myScanner.next());
-        System.out.println(myScanner.next());*/
-        /*System.out.println(myScanner.nextLine());
-        System.out.println(myScanner.hasNext());
-        System.out.println(myScanner.nextInt());
-        System.out.println(myScanner.hasNextInt());*/
+        System.out.print(myScanner.next());
+        System.out.print(myScanner.next());
+        System.out.print(myScanner.next());
+        System.out.print(myScanner.next());
+        System.out.print(myScanner.next());
+//        System.out.println(myScanner.nextLine());
+//        System.out.println(myScanner.nextLine());
+//        System.out.println(myScanner.hasNext());
+//        System.out.println(myScanner.nextInt());
+//        System.out.println(myScanner.hasNextInt());
 
-        // uncomment to test scanner using file of ints
-        /*my_scanner myScanner1 = new my_scanner("C:/Users/ARTEM/IdeaProjects/ACP12/ArtemBrytchenko/src/usable_files/int_test");
 
-        System.out.println(myScanner1.next());
-        System.out.println(myScanner1.nextLine());
-        System.out.println(myScanner1.nextInt());
-        System.out.println(myScanner1.hasNext());
-        System.out.println(myScanner1.hasNextInt());*/
-
-        // uncomment to test scanner using console
-        /*my_scanner myScanner2 = new my_scanner(System.in);
-
-        System.out.println(myScanner2.next());
-        System.out.println(myScanner2.nextLine());
-        System.out.println(myScanner2.hasNext());
-        System.out.println(myScanner2.nextInt());
-        System.out.println(myScanner2.hasNextInt());*/
     }
 }
