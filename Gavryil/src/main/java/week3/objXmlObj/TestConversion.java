0f2fc0d4be6cfd276d540f3bc74d4b0f43460ee9
kroshkamedved <lokhmytko.gavriil@gmail.com>
2016-03-07 00:04:24 +0200

    Add Junit

--- Gavryil/src/main/java/week3/objXmlObj/TestConversion.java
+++ Gavryil/src/main/java/week3/objXmlObj/TestConversion.java
@@ -1,10 +1,32 @@
 package week3.objXmlObj;
 
+import org.xml.sax.SAXException;
+import week3.domParsing.DomParsingUtils;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by gavri on 06.03.2016.
  */
 public class TestConversion {
-    public static void main(String[] args) {
-    TestObject object = new TestObject("Me",24,700,"Kyiv");
+
+    public static final String XML_PATH = "Gavryil/src/main/resources/TestObject.xml";
+
+    public static void main(String[] args) throws InstantiationException, IllegalAccessException, ParserConfigurationException, IOException, SAXException {
+        TestObject object = new TestObject("Me",24,700,"Kyiv");
+
+        String xmlObject = object.toXml();
+        DomParsingUtils.stringToXml(xmlObject, XML_PATH);
+
+        String result = DomParsingUtils.parseDoc(DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File(XML_PATH)));
+        System.out.println(result);
+
+        TestObject clone = new TestObject();
+        clone.getFieldsFromXml(new File(XML_PATH));
+        System.out.println("****************");
+        System.out.println(clone);
     }
 }
--- Gavryil/src/main/java/week3/objXmlObj/TestObject.java
+++ Gavryil/src/main/java/week3/objXmlObj/TestObject.java
@@ -1,9 +1,22 @@
 package week3.objXmlObj;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
 /**
  * Created by gavri on 06.03.2016.
  */
 public class TestObject {
+
     private String name;
     private int age;
     private int salary;
@@ -51,11 +64,72 @@ public class TestObject {
         this.address = address;
     }
 
-    public String toXml(){
+    public String toXml() throws IllegalAccessException, InstantiationException {
         StringBuilder builder = new StringBuilder();
-        builder.append("<"+getClass().getSimpleName()+">\n");
+        builder.append("<" + getClass().getSimpleName() + ">\n");
+        Class cl = this.getClass();
+        if (name != null) {
 
-        builder.append("</"+getClass().getSimpleName()+">");
+            Field[] fields = cl.getDeclaredFields();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                builder.append("<" + field.getName() + ">" + field.get(this) + "</" + field.getName() + ">\n");
+            }
+
+        }
+        builder.append("</" + getClass().getSimpleName() + ">");
         return builder.toString();
+    }
+
+    public void getFieldsFromXml(File file) throws ParserConfigurationException, IOException, SAXException {
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
+        Element root = doc.getDocumentElement();
+        NodeList list = root.getChildNodes();
+        for (int i = 0; i < list.getLength(); i++) {
+            Node node = list.item(i);
+            if (node.getNodeName().equals("name")) {
+                this.name = node.getTextContent();
+            } else if (node.getNodeName().equals("age")) {
+                this.age = Integer.parseInt(node.getTextContent());
+            } else if (node.getNodeName().equals("salary")) {
+                this.salary = Integer.parseInt(node.getTextContent());
+            } else if (node.getNodeName().equals("address")) {
+                this.address = node.getTextContent();
+            }
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "TestObject{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TestObject that = (TestObject) o;
+
+        if (age != that.age) return false;
+        if (salary != that.salary) return false;
+        if (!name.equals(that.name)) return false;
+        return address.equals(that.address);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        result = 31 * result + salary;
+        result = 31 * result + address.hashCode();
+        return result;
     }
 }
