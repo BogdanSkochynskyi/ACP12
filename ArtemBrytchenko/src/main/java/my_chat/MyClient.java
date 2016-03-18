1042f8f7a183c59196a5e4dc9eb8a10248c3f77f
BrArty <brtartemiy@ukr.net>
2016-03-18 16:05:59 +0200

    safe closing, forward messages from server

--- ArtemBrytchenko/src/main/java/my_chat/MyClient.java
+++ ArtemBrytchenko/src/main/java/my_chat/MyClient.java
@@ -1,26 +1,102 @@
 package my_chat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class MyClient{
-    public MyClient(){
+public class MyClient {
+    private String name;
+
+    public MyClient() {
         try {
             Socket client = new Socket("localhost", 7777);
-            PrintWriter pw = new PrintWriter(client.getOutputStream(), true);
-            Scanner sc = new Scanner(System.in);
+            Scanner readName = new Scanner(System.in);
+            System.out.println("Type your name: ");
+            name = readName.nextLine();
 
-            String text = "";
-            while (!text.equals("exit")) {
-                text = sc.nextLine();
-                pw.println(text);
-            }
-//// TODO: safe close of client without exception.
-            client.close();
+            new SendMessage(client).start();
+            new GetMessage(client).start();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private class SendMessage extends Thread {
+        private PrintWriter pw;
+        private Scanner sc;
+        private Socket client;
+
+        public SendMessage(Socket client) throws IOException {
+            this.client = client;
+        }
+
+        public void close(){
+            try {
+                pw.close();
+                sc.close();
+                client.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            sc = new Scanner(System.in);
+            while (true) {
+                try {
+                    pw = new PrintWriter(client.getOutputStream(), true);
+                    pw.println(name);
+
+                    String text;
+
+                    text = sc.nextLine();
+                    if (text.equals("exit")) {
+                        break;
+                    }
+                    pw.println(text);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    close();
+                }
+            }
+        }
+    }
+
+    private class GetMessage extends Thread {
+        private BufferedReader br;
+        private Socket client;
+
+        public GetMessage(Socket client) {
+            this.client = client;
+        }
+
+        public void close(){
+            try {
+                br.close();
+                client.close();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            boolean isConnected = true;
+            while (isConnected) {
+                try {
+                    br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    System.out.println(br.readLine());
+                } catch (IOException e) {
+                    close();
+                    isConnected = false;
+                    System.err.println("Server was shut down");
+                }
+            }
         }
     }
 }
--- ArtemBrytchenko/src/main/java/my_chat/MyServer.java
+++ ArtemBrytchenko/src/main/java/my_chat/MyServer.java
@@ -3,38 +3,79 @@ package my_chat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
-public class MyServer{
-    private BufferedReader br;
+public class MyServer {
+    private List<Socket> clientList = Collections.synchronizedList(new ArrayList<>());
 
     public MyServer(int port) throws IOException {
         try {
             ServerSocket server = new ServerSocket(port);
             while (true) {
                 Socket client = server.accept();
-                br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-                new GetMessage().start();
+                System.out.printf("new client was connected: IP %s, port %s\n",
+                        client.getInetAddress(), client.getPort());
+                new ForwardMessage(client).start();
+                clientList.add(client);
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private class GetMessage extends Thread{
-        @Override
-        public void run() {
-            try{
-                String text;
-                while ((text = br.readLine()) != null && !text.equals("exit")) {
-                    System.out.println(text);
+    private class ForwardMessage extends Thread {
+        private BufferedReader br;
+        private PrintWriter pw;
+        private Socket client;
+        private boolean setBlock = true;
+
+        public ForwardMessage(Socket client) {
+            this.client = client;
+        }
+
+        public void close() {
+            try {
+                br.close();
+                pw.close();
+                Iterator<Socket> iter = clientList.iterator();
+                while (iter.hasNext()){
+                    iter.next().close();
                 }
             } catch (IOException e) {
-                e.getMessage();
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void run() {
+            while (setBlock) {
+                try {
+                    br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                    String name = br.readLine();
+                    String text;
+                    text = br.readLine();
+                    if (text.equals("exit")) {
+                        break;
+                    }
+                    Iterator<Socket> iter = clientList.iterator();
+                    while (iter.hasNext()) {
+                        pw = new PrintWriter(iter.next().getOutputStream(), true);
+                        pw.println(name + " : " + text);
+                    }
+                } catch (IOException e) {
+                    e.getMessage();
+                    setBlock = false;
+                } finally {
+                    close();
+                }
             }
         }
     }
-
 }
 
